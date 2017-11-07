f77c1e78863edb432e76a34c500d146312c088fc
gitaroktato <gitaroktato@gmail.com>
2017-11-07 11:41:45 +0100

    Added first commit with Cassandra driver

---
+++ src/main/java/com/example/demo/CustomerContext.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+/**
+ * Created by Oresztesz_Margaritis on 11/6/2017.
+ */
+public class CustomerContext {
+}
---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloController {
+
+//    @Autowired
+//    private CustomerContext ctx;
+
+    @RequestMapping(value = "/api")
+    public String hello(@RequestHeader(value="Customer-Context") String customerContext) {
+        System.out.println(customerContext);
+        return "OK";
+    }
+}
