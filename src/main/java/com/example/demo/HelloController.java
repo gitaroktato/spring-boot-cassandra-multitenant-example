df25ebb57a12442729cc6911ce71f53d5aead9a6
gitaroktato <gitaroktato@gmail.com>
2017-11-14 08:35:12 +0100

    Removed explicit Header handling from controller

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,25 +3,20 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class HelloController {
-
-    @Autowired
-    private CustomerContext ctx;
 
     @Autowired
     private UserRepository userRepo;
 
     @RequestMapping(value = "/api")
-    public ResponseEntity<String> hello(
-            @RequestHeader(value="Customer-Context") String customerContext) {
-        ctx.setCustomerContext(customerContext);
-        User user = userRepo.findByUsername("Bob");
+    public ResponseEntity<String> hello(@RequestParam String username) {
+        User user = userRepo.findByUsername(username);
         return new ResponseEntity<>(user.getUsername(), HttpStatus.OK);
     }
 }
