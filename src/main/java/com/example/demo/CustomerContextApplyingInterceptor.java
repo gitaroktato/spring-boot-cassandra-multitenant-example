496998a465f70337f85c1d48473883f08472e570
gitaroktato <gitaroktato@gmail.com>
2017-11-14 09:07:22 +0100

    Weaving customer-context with annotation.

--- src/main/java/com/example/demo/CustomerContextApplyingInterceptor.java
+++ src/main/java/com/example/demo/CustomerContextApplyingInterceptor.java
@@ -1,24 +1,24 @@
 package com.example.demo;
 
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.cassandra.core.CassandraTemplate;
 import org.springframework.stereotype.Component;
 
+@Aspect
 @Component
-public class CustomerContextApplyingInterceptor implements MethodInterceptor {
+public class CustomerContextApplyingInterceptor {
 
     @Autowired
     private CustomerContext ctx;
     @Autowired
     private CassandraTemplate template;
 
-    @Override
-    public Object invoke(MethodInvocation i) throws Throwable {
+    @Before("execution(public * org.springframework.data.repository.Repository+.*(..))")
+    public void setCustomerContext() throws Throwable {
         template.execute("USE " + ctx.getCustomerContext());
         System.out.println("Customer context is: " + ctx.getCustomerContext());
-        return i.proceed();
     }
 
 }
--- src/main/java/com/example/demo/MvcConfig.java
+++ src/main/java/com/example/demo/MvcConfig.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
