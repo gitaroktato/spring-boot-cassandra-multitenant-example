718eb1949d67b8d7c66bd5d31458600183d7ec14
gitaroktato <gitaroktato@gmail.com>
2017-11-21 08:32:10 +0100

    Injecting customer context

--- src/main/java/com/example/demo/repository/KeyspaceAwareCassandraRepository.java
+++ src/main/java/com/example/demo/repository/KeyspaceAwareCassandraRepository.java
@@ -1,18 +1,18 @@
 package com.example.demo.repository;
 
+import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.datastax.driver.core.querybuilder.Select;
+import com.example.demo.CustomerContext;
+import org.springframework.cassandra.core.cql.CqlIdentifier;
 import org.springframework.data.cassandra.core.CassandraOperations;
 import org.springframework.data.cassandra.repository.query.CassandraEntityInformation;
-import org.springframework.data.cassandra.repository.support.SimpleCassandraRepository;
 
 import java.io.Serializable;
 import java.util.List;
 
-/**
- * Created by Oresztesz_Margaritis on 11/19/2017.
- */
 public class KeyspaceAwareCassandraRepository<T, ID extends Serializable>
-        extends SimpleCassandraRepository<T, ID>  {
+
+    private CustomerContext ctx;
 
     private final CassandraEntityInformation<T, ID> metadata;
     private final CassandraOperations operations;
@@ -28,7 +28,19 @@ public class KeyspaceAwareCassandraRepository<T, ID extends Serializable>
     @Override
     public T findOne(ID id) {
         System.out.println("ID: == " + id);
-        return super.findOne(id);
+
+        CqlIdentifier primaryKey = operations.getConverter()
+                .getMappingContext()
+                .getPersistentEntity(metadata.getJavaType())
+                .getIdProperty().getColumnName();
+
+        Select select = QueryBuilder.select().all()
+                .from("customer1",
+                        metadata.getTableName().toCql())
+                .where(QueryBuilder.eq(primaryKey.toString(), id))
+                .limit(1);
+
+        return operations.selectOne(select, metadata.getJavaType());
     }
 
     @Override
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,9 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by Oresztesz_Margaritis on 11/9/2017.
- */
 public interface UserRepository extends CrudRepository<User, String> {
     @Override
     User findOne(String username);
