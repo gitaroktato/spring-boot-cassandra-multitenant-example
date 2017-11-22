ac4269579f9e9656fdd49a4214dff7e7e95360ab
gitaroktato <gitaroktato@gmail.com>
2017-11-22 08:12:34 +0100

    Working solution

--- src/main/java/com/example/demo/repository/KeyspaceAwareCassandraRepository.java
+++ src/main/java/com/example/demo/repository/KeyspaceAwareCassandraRepository.java
@@ -2,11 +2,17 @@ package com.example.demo.repository;
 
 import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.datastax.driver.core.querybuilder.Select;
+import com.example.demo.CustomerContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cassandra.core.cql.CqlIdentifier;
 import org.springframework.data.cassandra.core.CassandraOperations;
 import org.springframework.data.cassandra.repository.query.CassandraEntityInformation;
 import org.springframework.data.cassandra.repository.support.SimpleCassandraRepository;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
+import javax.servlet.ServletContext;
 import java.io.Serializable;
 import java.util.List;
 
@@ -16,6 +22,9 @@ public class KeyspaceAwareCassandraRepository<T, ID extends Serializable>
     private final CassandraEntityInformation<T, ID> metadata;
     private final CassandraOperations operations;
 
+    @Autowired
+    private CustomerContext ctx;
+
     public KeyspaceAwareCassandraRepository(
             CassandraEntityInformation<T, ID> metadata,
             CassandraOperations operations) {
@@ -24,17 +33,23 @@ public class KeyspaceAwareCassandraRepository<T, ID extends Serializable>
         this.operations = operations;
     }
 
+    private ServletContext getServletContext() {
+        return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+                .getRequest().getServletContext();
+    }
+
     @Override
     public T findOne(ID id) {
         System.out.println("ID: == " + id);
-
+        SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this,
+                getServletContext());
         CqlIdentifier primaryKey = operations.getConverter()
                 .getMappingContext()
                 .getPersistentEntity(metadata.getJavaType())
                 .getIdProperty().getColumnName();
 
         Select select = QueryBuilder.select().all()
-                .from("customer1",
+                .from(ctx.getCustomerContext(),
                         metadata.getTableName().toCql())
                 .where(QueryBuilder.eq(primaryKey.toString(), id))
                 .limit(1);
